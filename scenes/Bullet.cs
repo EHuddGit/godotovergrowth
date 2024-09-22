2871f3ac1c58a4ab8df9660efbb4587fb3025e76
ethan <e.huddoffical@gmail.com>
2024-09-22 02:21:28 -0700

    got bullets spawning and moving. bullet spawning does get off from animation the longer the shooting goes on also requires a way to delete bullets after leaving the screen

---
+++ scenes/Bullet.cs
@@ -0,0 +1,22 @@
+using Godot;
+using System;
+
+public partial class Bullet : Node2D
+{
+	// Called when the node enters the scene tree for the first time.
+	private int speed = 300;
+	[Export]
+	public float direction;
+	public override void _PhysicsProcess(double delta)
+	{
+		if(direction > 0)
+		{
+			GetNode<AnimatedSprite2D>("bulletBody").FlipH = false;
+		}
+		else if(direction < 0)
+		{
+			GetNode<AnimatedSprite2D>("bulletBody").FlipH = true;
+		}
+		MoveLocalX(direction * speed * (float)delta);
+	}
+}
--- scripts/Player.cs
+++ scripts/Player.cs
@@ -1,51 +1,119 @@
 using Godot;
 using System;
 
+//need to move all animations to process and take them out of physics process
+// timing of the sprite shooting the bullet and the bullet coming out seems off
+//bullet sprite needs to be flipped
 public partial class Player : CharacterBody2D
 {
 	public const float Speed = 300.0f;
 	public const float JumpVelocity = -400.0f;
+    enum States {IDLE,WALKING,SHOOTING,RUNNING};
+	private States current = States.IDLE;
+	private bool finishedShot = false;
+	private bool bulletFired = false;
+	private PackedScene bullet = ResourceLoader.Load<PackedScene>("res://scenes/bullet.tscn");
+	public void stateChange(float direction)
+	{
+		
+		if(direction != 0 && ((current == States.SHOOTING && finishedShot == true) || current != States.SHOOTING))
+			current = States.WALKING;
+		else if(Input.IsActionPressed("shoot"))
+			current = States.SHOOTING;
+		else if((current == States.SHOOTING && finishedShot == true) || current != States.SHOOTING)
+		{
+			current = States.IDLE;
+			finishedShot = false;
+		}
+	}
 
+	public void animations(float direction)
+	{
+		AnimatedSprite2D playerSprite = GetNode<AnimatedSprite2D>("playerBody");
+		Marker2D muzzle = GetNode<Marker2D>("muzzle");
+		// flips the sprite based on which direction the player is moving
+		if(direction > 0)
+		{
+			playerSprite.FlipH = false;
+		}
+		else if(direction < 0)
+		{
+			playerSprite.FlipH = true;
+		}
+		//playing which animation based on the state
+		if(current == States.SHOOTING)
+			playerSprite.Play("attack");
+		else if(current == States.WALKING)
+			playerSprite.Play("walk");
+		else
+			playerSprite.Play("idle");
+	}
+	public void animationFinish()
+	{
+		finishedShot = true;
+		if(bulletFired == true)
+			bulletFired = false;
+		GD.Print("animation is finished\n");
+	}
+	public void bulletspawn()
+	{
+		int direction = 0;
+		if(bulletFired == false)
+		{
+			GD.Print("bullet spawned!\n");
+			Marker2D muzzle = GetNode<Marker2D>("muzzle");
+			Vector2 muzzlePosition = muzzle.GlobalPosition;
+			var bullet_instance = bullet.Instantiate();
+
+			GD.Print(GetNode<AnimatedSprite2D>("playerBody").FlipH);
+			if(GetNode<AnimatedSprite2D>("playerBody").FlipH)
+			{
+				direction = -1;
+				GD.Print(muzzlePosition.X);
+				muzzlePosition.X  -= 1.75F * muzzle.Position.X;
+			}
+			else
+			{
+				direction = 1;
+				GD.Print(muzzlePosition.X);
+			}
+			(bullet_instance as Bullet).direction = direction;
+
+			(bullet_instance as Node2D).GlobalPosition = muzzlePosition;
+			GetParent().AddChild(bullet_instance);
+			bulletFired = true;
+		}
+	}
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
 		float direction = Input.GetAxis("left","right");
-		AnimatedSprite2D playerSprite = GetNode<AnimatedSprite2D>("playerBody");
+		
 		// Add the gravity.
 		if (!IsOnFloor())
 		{
 			velocity += GetGravity() * (float)delta;
 		}
-		// flips the sprite based on which direction the player is moving
-		if(direction > 0)
-			playerSprite.FlipH = false;
-		else if(direction < 0)
-			playerSprite.FlipH = true;
 
-		if(direction != 0)
-			playerSprite.Play("walk");
-		else if(Input.IsActionPressed("shoot"))
-			{
-				playerSprite.Play("attack");
-				GD.Print("attacking\n");
-			}
-		else
-			{
-				playerSprite.Play("idle");
-				GD.Print("idling\n");
-			}
+		stateChange(direction);
+		animations(direction);
+		if(current == States.SHOOTING)
+			bulletspawn();
 
 		if (direction != 0)
-		{
-			
 			velocity.X = direction * Speed;
-		}
 		else
-		{
 			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-		}
 
 		Velocity = velocity;
 		MoveAndSlide();
 	}
+
+	// public override void _PhysicsProcess(double delta)
+	// {
+	// 	//input to signal state change
+	// 	//change the state if needed
+	// 	//process the appropiate actions for current state
+	// }
+	
 }
